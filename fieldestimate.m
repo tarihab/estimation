4e07bfb003414e89bee935bc30b9e99ca1237ad1
tarihab <tarihab@gmail.com>
2019-05-13 12:25:03 +0530

    update-12.24-13.05.19

--- fieldestimate.m
+++ fieldestimate.m
@@ -2,13 +2,16 @@ function f = fieldestimate(x, y, xc, yc, sigma, a)
 % evaluate the estimated value of the scalar field at point (x,y)
 
 	p = length(xc);
+	phi = zeros(1,p);
 	
 	f = 0;
-	for i=1:p
-		phi = exp((-(x-xc)^2 - (y-yc)^2)/(2*sigma*sigma));
-		phi = phi*(1/(sigma*sqrt(2*pi)));
+	parfor i=1:p
+		phi(i) = exp((-(x-xc(i))^2 - (y-yc(i))^2)/(2*sigma*sigma));
+		phi(i) = phi(i)*(1/(sigma*sqrt(2*pi)));
 
-		f = f + phi*a(i);
+		phi(i) = a(i)*phi(i);
 	end
+
+	f = sum(phi);
 
 end
